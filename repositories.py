f737b9e5e94307d05b56f5707e350e82c945c633
HelgeCPH <ropf@itu.dk>
2020-02-04 23:23:22 +0100

    Added file for student group repos

---
+++ repositories.py
@@ -0,0 +1,57 @@
+GROUP_REPOS = [
+    [
+        "group a",
+        "<master>",
+        ["https://github.com/<A>/<B>", "https://github.com/<A>/<C>", "etc."],
+    ],
+    [
+        "group b",
+        "2k20 MSc",
+        ["https://github.com/<B>/<B>", "https://github.com/<B>/<C>", "etc."],
+    ],
+    [
+        "group c",
+        "U buntu?",
+        ["https://github.com/<C>/<B>", "https://github.com/<C>/<C>", "etc."],
+    ],
+    [
+        "group d",
+        "DevOops",
+        ["https://github.com/<D>/<B>", "https://github.com/<D>/<C>", "etc."],
+    ],
+    [
+        "group e",
+        "LazyOps - Semi manual CI/CD",
+        ["https://github.com/<E>/<B>", "https://github.com/<E>/<C>", "etc."],
+    ],
+    [
+        "group f",
+        "Backstreet NoobBois",
+        ["https://github.com/<F>/<B>", "https://github.com/<F>/<C>", "etc."],
+    ],
+    [
+        "group e",
+        "LearnIT > GitHub",
+        ["https://github.com/<G>/<B>", "https://github.com/<G>/<C>", "etc."],
+    ],
+    [
+        "group g",
+        "Fuld smadder",
+        ["https://github.com/<H>/<B>", "https://github.com/<H>/<C>", "etc."],
+    ],
+    [
+        "group h",
+        "TB",
+        ["https://github.com/<I>/<B>", "https://github.com/<I>/<C>", "etc."],
+    ],
+    [
+        "group i",
+        "DevUp (and sometime down)",
+        ["https://github.com/<J>/<B>", "https://github.com/<J>/<C>", "etc."],
+    ],
+    [
+        "group j",
+        "DevOps drengene",
+        ["https://github.com/<K>/<B>", "https://github.com/<K>/<C>", "etc."],
+    ],
+]
