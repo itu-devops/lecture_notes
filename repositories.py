e6e720948be5308227d0b4c453f527c039b27e32
Marek Kisiel <mr.kislun@gmail.com>
2020-02-06 12:32:12 +0100

    Group LazyOps

--- repositories.py
+++ repositories.py
@@ -22,7 +22,7 @@ GROUP_REPOS = [
     [
         "group e",
         "LazyOps - Semi manual CI/CD",
-        ["https://github.com/<E>/<B>", "https://github.com/<E>/<C>", "etc."],
+        ["https://github.com/itu-devops/2020-spring"],
     ],
     [
         "group f",
