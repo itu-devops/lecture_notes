68417c3398b5be7b1f06fbc984b69c99a2a7011e
HelgeCPH <ropf@itu.dk>
2020-02-17 17:08:06 +0100

    Fixed double group register

--- repositories.py
+++ repositories.py
@@ -1,67 +1,26 @@
 GROUP_REPOS = [
-    [
-        "group a",
-        "<master>",
-        ["https://github.com/janschill/uni-devops"],
-    ],
-    [
-        "group b",
-        "2k20 MSc",
-        ["https://github.com/Trivivium/devops-2k20"],
-    ],
-    [
-        "group c",
-        "U buntu?",
-        ["https://github.com/kindaninja/minitwit"],
-    ],
-    [
-        "group d",
-        "DevOops",
-        ["https://github.com/jlndk/devoops" ],
-    ],
+    ["group a", "<master>", ["https://github.com/janschill/uni-devops"]],
+    ["group b", "2k20 MSc", ["https://github.com/Trivivium/devops-2k20"]],
+    ["group c", "U buntu?", ["https://github.com/kindaninja/minitwit"]],
+    ["group d", "DevOops", ["https://github.com/jlndk/devoops"]],
     [
         "group e",
         "LazyOps - Semi manual CI/CD",
         ["https://github.com/banksdev/minitwit"],
     ],
-    [
-        "group f",
-        "Backstreet NoobBois",
-        ["https://github.itu.dk/mosh/DevOps"],
-    ],
-    [
-        "group g",
-        "LearnIT > GitHub",
-        ["https://github.itu.dk/mahn/OneDevOps"],
-    ],
-    [
-        "group h",
-        "Fuld smadder",
-        ["https://github.com/matt035343/devops"],
-    ],
-    [
-        "group i",
-        "TB",
-        ["https://github.itu.dk/PHIJ/DevOps"],
-    ],
+    ["group f", "Backstreet NoobBois", ["https://github.itu.dk/mosh/DevOps"]],
+    ["group g", "LearnIT > GitHub", ["https://github.itu.dk/mahn/OneDevOps"]],
+    ["group h", "Fuld smadder", ["https://github.com/matt035343/devops"]],
+    ["group i", "TB", ["https://github.itu.dk/PHIJ/DevOps"]],
     [
         "group j",
-        "DevUp (and sometime down)",
-        ["https://github.com/<J>/<B>", "https://github.com/<J>/<C>", "etc."],
+        "noname",
+        ["https://github.com/mortenskoett/itu-devops-group-noname"],
     ],
     [
         "group k",
         "DevOps drengene",
         ["https://github.com/DevOps-Drengene/DevOps-Project"],
     ],
-    [
-        "group l",
-        "Blanao",
-        ["https://github.com/alexander34ro/DevOps"],
-    ],
-    [
-        "group m",
-        "noname",
-        ["https://github.com/mortenskoett/itu-devops-group-noname"],
-    ]
+    ["group l", "Blanao", ["https://github.com/alexander34ro/DevOps"]],
 ]
