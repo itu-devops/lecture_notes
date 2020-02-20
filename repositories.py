0299308cbd0075e060a4206252764e5e6c584852
Lasse Agersten <lage@itu.dk>
2020-02-20 16:03:37 +0100

    Update repositories.py

--- repositories.py
+++ repositories.py
@@ -58,7 +58,7 @@ GROUP_REPOS = [
     [
         "group i",
         "TB",
-        ["https://github.itu.dk/minitwit-tdb/DevOps"],
+        ["https://github.itu.dk/minitwit-tdb/DevOps", "https://github.com/minitwit-tdb/DevOps"],
         "http://157.245.35.115",
         "http://157.245.35.115:5001",
     ],
