1ecf88e260709abf5ff549dc53b47c14e4835ca6
Lasse Agersten <lage@itu.dk>
2020-02-20 12:17:24 +0100

    Update repositories.py

--- repositories.py
+++ repositories.py
@@ -59,8 +59,8 @@ GROUP_REPOS = [
         "group i",
         "TB",
         ["https://github.itu.dk/PHIJ/DevOps"],
-        "http://<minitwit_application_url>",
-        "http://<sim_api_url>",
+        "http://157.245.35.115",
+        "http://157.245.35.115:5001",
     ],
     [
         "group j",
