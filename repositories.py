e5f946a58dd5ff4715fd912d603d5d9b8f239d43
Toheed Sharif <info@toheed.dk>
2020-02-24 11:46:28 +0100

    Update repositories.py

--- repositories.py
+++ repositories.py
@@ -38,8 +38,8 @@ GROUP_REPOS = [
         "group f",
         "Backstreet NoobBois",
         ["https://github.itu.dk/mosh/minitwitgo", "https://github.com/MoToSh99/MiniTwit"],
-        "http://46.101.166.100/:5000/",
-        "http://46.101.166.100/:5001/",
+        "http://46.101.166.100:5000/",
+        "http://46.101.166.100:5001/",
     ],
     [
         "group g",
