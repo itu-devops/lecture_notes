b81386fa61278d68a78b8470eacd8131c6bdcfff
HelgeCPH <ropf@itu.dk>
2020-03-05 13:01:48 +0100

    Added file for URL collection

---
+++ misc_urls.py
@@ -0,0 +1,74 @@
+GROUP_URLS = [
+    [
+        "group a",
+        "<master>",
+        # Monitoring URL:
+        "http://<path_to_your_monitoring_dashboard>",
+    ],
+    [
+        "group b",
+        "2k20 MSc",
+        # Monitoring URL:
+        "http://<path_to_your_monitoring_dashboard>",
+    ],
+    [
+        "group c",
+        "U buntu?",
+        # Monitoring URL:
+        "http://<path_to_your_monitoring_dashboard>",
+    ],
+    [
+        "group d",
+        "DevOops",
+        # Monitoring URL:
+        "http://<path_to_your_monitoring_dashboard>",
+    ],
+    [
+        "group e",
+        "LazyOps - Semi manual CI/CD",
+        # Monitoring URL:
+        "http://<path_to_your_monitoring_dashboard>",
+    ],
+    [
+        "group f",
+        "Backstreet NoobBois",
+        # Monitoring URL:
+        "http://<path_to_your_monitoring_dashboard>",
+    ],
+    [
+        "group g",
+        "LearnIT > GitHub",
+        # Monitoring URL:
+        "http://<path_to_your_monitoring_dashboard>",
+    ],
+    [
+        "group h",
+        "Fuld smadder",
+        # Monitoring URL:
+        "http://<path_to_your_monitoring_dashboard>",
+    ],
+    [
+        "group i",
+        "TB",
+        # Monitoring URL:
+        "http://<path_to_your_monitoring_dashboard>",
+    ],
+    [
+        "group j",
+        "noname",
+        # Monitoring URL:
+        "http://<path_to_your_monitoring_dashboard>",
+    ],
+    [
+        "group k",
+        "DevOps drengene",
+        # Monitoring URL:
+        "http://<path_to_your_monitoring_dashboard>",
+    ],
+    [
+        "group l",
+        "Blanao",
+        # Monitoring URL:
+        "http://<path_to_your_monitoring_dashboard>",
+    ],
+]
