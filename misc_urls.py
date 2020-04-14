a26d6c29e1ef33efc29d0e20eda124cf26aa6c19
HelgeCPH <ropf@itu.dk>
2020-04-14 23:45:38 +0200

    Added placeholder for report URL

--- misc_urls.py
+++ misc_urls.py
@@ -4,71 +4,95 @@ GROUP_URLS = [
         "<master>",
         # Monitoring URL:
         "http://janschill.de:3000",
+        # Security report URL:
+        "http://<path>/<to>/<your>/<report>",
     ],
     [
         "group b",
         "2k20 MSc",
         # Monitoring URL:
         "http://134.209.245.96:3000",
+        # Security report URL:
+        "http://<path>/<to>/<your>/<report>",
     ],
     [
         "group c",
         "U buntu?",
         # Monitoring URL:
         "http://minitwit.dk/monitor",
+        # Security report URL:
+        "http://<path>/<to>/<your>/<report>",
     ],
     [
         "group d",
         "DevOops",
         # Monitoring URL:
         "https://grafana.minitwit.tk",
+        # Security report URL:
+        "http://<path>/<to>/<your>/<report>",
     ],
     [
         "group e",
         "LazyOps - Semi manual CI/CD",
         # Monitoring URL:
         "http://207.154.252.199:3000/",
+        # Security report URL:
+        "http://<path>/<to>/<your>/<report>",
     ],
     [
         "group f",
         "Backstreet NoobBois",
         # Monitoring URL:
         "http://134.122.83.72:3000/",
+        # Security report URL:
+        "http://<path>/<to>/<your>/<report>",
     ],
     [
         "group g",
         "LearnIT > GitHub",
         # Monitoring URL:
         "http://104.248.246.24:9111",
+        # Security report URL:
+        "http://<path>/<to>/<your>/<report>",
     ],
     [
         "group h",
         "Fuld smadder",
         # Monitoring URL:
         "http://104.248.88.157:3000/",
+        # Security report URL:
+        "http://<path>/<to>/<your>/<report>",
     ],
     [
         "group i",
         "TB",
         # Monitoring URL:
         "http://http://157.245.35.115:4000/login",
+        # Security report URL:
+        "http://<path>/<to>/<your>/<report>",
     ],
     [
         "group j",
         "noname",
         # Monitoring URL:
         "http://46.101.215.40:3000/",
+        # Security report URL:
+        "http://<path>/<to>/<your>/<report>",
     ],
     [
         "group k",
         "DevOps drengene",
         # Monitoring URL:
         "http://157.245.27.128:3001",
+        # Security report URL:
+        "http://<path>/<to>/<your>/<report>",
     ],
     [
         "group l",
         "Blanao",
         # Monitoring URL:
         "https://dashboard.heroku.com/apps/minitwit-simulator-api/metrics/web?ending=0-hours-ago&starting=2-hours-ago",
+        # Security report URL:
+        "http://<path>/<to>/<your>/<report>",
     ],
 ]
