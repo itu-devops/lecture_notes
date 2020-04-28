b09be6454d491e55af04477f436a5aea82225a07
HelgeCPH <ropf@itu.dk>
2020-04-28 16:54:53 +0200

    Added file for linking final reports

---
+++ final_report_urls.py
@@ -0,0 +1,74 @@
+REPORT_URLS = [
+    [
+        "group a",
+        "<master>",
+        # Report Release URL:
+        "http://path/to/your/report.zip",
+    ],
+    [
+        "group b",
+        "2k20 MSc",
+        # Report Release URL:
+        "http://path/to/your/report.zip",
+    ],
+    [
+        "group c",
+        "U buntu?",
+        # Report Release URL:
+        "http://path/to/your/report.zip",
+    ],
+    [
+        "group d",
+        "DevOops",
+        # Report Release URL:
+        "http://path/to/your/report.zip",
+    ],
+    [
+        "group e",
+        "LazyOps - Semi manual CI/CD",
+        # Report Release URL:
+        "http://path/to/your/report.zip",
+    ],
+    [
+        "group f",
+        "Backstreet NoobBois",
+        # Report Release URL:
+        "http://path/to/your/report.zip",
+    ],
+    [
+        "group g",
+        "LearnIT > GitHub",
+        # Report Release URL:
+        "http://path/to/your/report.zip",
+    ],
+    [
+        "group h",
+        "Fuld smadder",
+        # Report Release URL:
+        "http://path/to/your/report.zip",
+    ],
+    [
+        "group i",
+        "TB",
+        # Report Release URL:
+        "http://path/to/your/report.zip",
+    ],
+    [
+        "group j",
+        "noname",
+        # Report Release URL:
+        "http://path/to/your/report.zip",
+    ],
+    [
+        "group k",
+        "DevOps drengene",
+        # Report Release URL:
+        "http://path/to/your/report.zip",
+    ],
+    [
+        "group l",
+        "Blanao",
+        # Report Release URL:
+        "http://path/to/your/report.zip",
+    ],
+]
