f9fb4fea2e4152a0e54bad35b4adb803ac1196c8
Smilla <smdi@itu.dk>
2022-02-28 21:58:38 +0100

    Add IP for our minitwit server

--- repositories.py
+++ repositories.py
@@ -52,8 +52,8 @@ GROUP_REPOS = [
         "group h",
         "Continuous disintegration",
         ["https://github.com/Herover/itu-devops-h"],
-        "<minitwit_url>",
-        "<minitwit_api_url>",
+        "http://143.198.249.10/",
+        "http://143.198.249.10/api",
     ],
     [
         "group i",
