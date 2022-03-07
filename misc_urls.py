940864401bc8f6e09119b49ca22040ef841407d9
Helge <ropf@itu.dk>
2022-03-07 14:51:58 +0100

    Added this year's groups to file

--- misc_urls.py
+++ misc_urls.py
@@ -1,7 +1,7 @@
 GROUP_URLS = [
     [
         "group a",
-        "<name>",
+        "Gruppe_A",
         # Monitoring URL:
         "<monitoring_url>",
         # Security report URL:
@@ -14,8 +14,218 @@ GROUP_URLS = [
         "<sla_review_url>",
     ],
     [
-        "group a",
-        "<name>",
+        "group b",
+        "Group Bee",
+        # Monitoring URL:
+        "<monitoring_url>",
+        # Security report URL:
+        "<security_report_url>",
+        # Logging URL:
+        "<logging_url>",
+        # SLA Report:
+        "<sla_report_url>",
+        # SLA Review:
+        "<sla_review_url>",
+    ],
+    [
+        "group c",
+        "DevOpps",
+        # Monitoring URL:
+        "<monitoring_url>",
+        # Security report URL:
+        "<security_report_url>",
+        # Logging URL:
+        "<logging_url>",
+        # SLA Report:
+        "<sla_report_url>",
+        # SLA Review:
+        "<sla_review_url>",
+    ],
+    [
+        "group d",
+        "DevelOps",
+        # Monitoring URL:
+        "<monitoring_url>",
+        # Security report URL:
+        "<security_report_url>",
+        # Logging URL:
+        "<logging_url>",
+        # SLA Report:
+        "<sla_report_url>",
+        # SLA Review:
+        "<sla_review_url>",
+    ],
+    [
+        "group e",
+        "ASPirin",
+        # Monitoring URL:
+        "<monitoring_url>",
+        # Security report URL:
+        "<security_report_url>",
+        # Logging URL:
+        "<logging_url>",
+        # SLA Report:
+        "<sla_report_url>",
+        # SLA Review:
+        "<sla_review_url>",
+    ],
+    [
+        "group f",
+        "PayRespect",
+        # Monitoring URL:
+        "<monitoring_url>",
+        # Security report URL:
+        "<security_report_url>",
+        # Logging URL:
+        "<logging_url>",
+        # SLA Report:
+        "<sla_report_url>",
+        # SLA Review:
+        "<sla_review_url>",
+    ],
+    [
+        "group g",
+        "Call Of Duty BlackOps",
+        # Monitoring URL:
+        "<monitoring_url>",
+        # Security report URL:
+        "<security_report_url>",
+        # Logging URL:
+        "<logging_url>",
+        # SLA Report:
+        "<sla_report_url>",
+        # SLA Review:
+        "<sla_review_url>",
+    ],
+    [
+        "group h",
+        "Continuous disintegration",
+        # Monitoring URL:
+        "<monitoring_url>",
+        # Security report URL:
+        "<security_report_url>",
+        # Logging URL:
+        "<logging_url>",
+        # SLA Report:
+        "<sla_report_url>",
+        # SLA Review:
+        "<sla_review_url>",
+    ],
+    [
+        "group i",
+        "A-team",
+        # Monitoring URL:
+        "<monitoring_url>",
+        # Security report URL:
+        "<security_report_url>",
+        # Logging URL:
+        "<logging_url>",
+        # SLA Report:
+        "<sla_report_url>",
+        # SLA Review:
+        "<sla_review_url>",
+    ],
+    [
+        "group j",
+        "Root",
+        # Monitoring URL:
+        "<monitoring_url>",
+        # Security report URL:
+        "<security_report_url>",
+        # Logging URL:
+        "<logging_url>",
+        # SLA Report:
+        "<sla_report_url>",
+        # SLA Review:
+        "<sla_review_url>",
+    ],
+    [
+        "group k",
+        "DevUps",
+        # Monitoring URL:
+        "<monitoring_url>",
+        # Security report URL:
+        "<security_report_url>",
+        # Logging URL:
+        "<logging_url>",
+        # SLA Report:
+        "<sla_report_url>",
+        # SLA Review:
+        "<sla_review_url>",
+    ],
+    [
+        "group l",
+        "iBois",
+        # Monitoring URL:
+        "<monitoring_url>",
+        # Security report URL:
+        "<security_report_url>",
+        # Logging URL:
+        "<logging_url>",
+        # SLA Report:
+        "<sla_report_url>",
+        # SLA Review:
+        "<sla_review_url>",
+    ],
+    [
+        "group m",
+        "DeadlyDevOps",
+        # Monitoring URL:
+        "<monitoring_url>",
+        # Security report URL:
+        "<security_report_url>",
+        # Logging URL:
+        "<logging_url>",
+        # SLA Report:
+        "<sla_report_url>",
+        # SLA Review:
+        "<sla_review_url>",
+    ],
+    [
+        "group n",
+        "DevØps",
+        # Monitoring URL:
+        "<monitoring_url>",
+        # Security report URL:
+        "<security_report_url>",
+        # Logging URL:
+        "<logging_url>",
+        # SLA Report:
+        "<sla_report_url>",
+        # SLA Review:
+        "<sla_review_url>",
+    ],
+    [
+        "group o",
+        "TBD",
+        # Monitoring URL:
+        "<monitoring_url>",
+        # Security report URL:
+        "<security_report_url>",
+        # Logging URL:
+        "<logging_url>",
+        # SLA Report:
+        "<sla_report_url>",
+        # SLA Review:
+        "<sla_review_url>",
+    ],
+    [
+        "group q",
+        "<SplashOps>",
+        # Monitoring URL:
+        "<monitoring_url>",
+        # Security report URL:
+        "<security_report_url>",
+        # Logging URL:
+        "<logging_url>",
+        # SLA Report:
+        "<sla_report_url>",
+        # SLA Review:
+        "<sla_review_url>",
+    ],
+    [
+        "group r",
+        "Rhododevdron",
         # Monitoring URL:
         "<monitoring_url>",
         # Security report URL:
