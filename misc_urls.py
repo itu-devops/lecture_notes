ebdc5df58a999fdffd8aa4c76a6b0a5f6c4fba40
Mcfishflap <44869764+Mcfishflap@users.noreply.github.com>
2022-03-30 14:15:23 +0200

    Update misc_urls.py

--- misc_urls.py
+++ misc_urls.py
@@ -65,7 +65,7 @@ GROUP_URLS = [
         # Logging URL:
         "http://shittylogs.tk:5601",
         # SLA:
-        "<sla_url>",
+        "https://github.com/ChadIImus/Devoops/blob/master/ServiceLevelAgreement.md",
         # SLA Review:
         "<sla_review_url>",
     ],
