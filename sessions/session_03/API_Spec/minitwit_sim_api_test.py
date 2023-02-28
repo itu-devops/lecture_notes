116288eda7d42bec0d199d2725b4fa0e47c401bf
HelgeCPH <ropf@itu.dk>
2023-02-28 16:23:59 +0100

    Automatic release of teaching material.

--- sessions/session_03/API_Spec/minitwit_sim_api_test.py
+++ sessions/session_03/API_Spec/minitwit_sim_api_test.py
@@ -82,7 +82,7 @@ def test_get_latest_user_msgs():
     query = {'no': 20, 'latest': 3}
     url = f'{BASE_URL}/msgs/{username}'
     response = requests.get(url, headers=HEADERS, params=query)
-    assert response.status_code == 200
+    assert response.status_code == 204
 
     got_it_earlier = False
     for msg in response.json():
