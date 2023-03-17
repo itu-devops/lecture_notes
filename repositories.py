54408f6a8d82e5cc60d894ccada69b2e4fb760d9
lauritsbonde <lauritsbonde@hotmail.dk>
2023-03-17 13:38:41 +0100

    Updated group g url
    We have made our application run on https

--- repositories.py
+++ repositories.py
@@ -39,7 +39,7 @@ GROUP_REPOS = [
         "group g",
         "DevJanitors",
         ["https://github.com/Dev-Janitors/minitwit", ],
-        "http://104.248.101.163:3000",
+        "https://104.248.101.163",
         "http://104.248.101.163:2222",
     ],
     [
