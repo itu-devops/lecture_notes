8c07bbd6a8887cd13e09f7f2fd2661182124556b
Patrick Matthiesen <43612965+PatrickMatthiesen@users.noreply.github.com>
2023-03-28 17:45:26 +0200

    Update repositories.py

--- repositories.py
+++ repositories.py
@@ -24,8 +24,8 @@ GROUP_REPOS = [
         "group e",
         "Souffle",
         ["https://github.com/PatrickMatthiesen/Souffle-MiniTwit", ],
-        "http://68.183.209.28:5000",
-        "http://68.183.209.28:5000/api/Sim",
+        "http://souffle.nu",
+        "http://souffle.nu/api/Sim",
     ],
     [
         "group f",
