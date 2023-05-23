3db40d9ca0d6e310011c386c855b49947c81dff8
Oliver Holmberg Jørgensen <oliver.hj.1999@gmail.com>
2023-05-23 21:11:30 +0200

    Update final_report_urls.py

--- final_report_urls.py
+++ final_report_urls.py
@@ -19,9 +19,9 @@ REPORT_URLS = [
     ],
     [
         "group d",
-        "<name>",
+        "CICDont",
         # Report Release URL:
-        "https://github.com/<gh_id>/<proj_id>/archive/refs/tags/<version_tag>.zip",
+        "https://github.com/DevOps-CI-CDont/DevOps-CI-CDont/archive/refs/tags/Exam.zip",
     ],
     [
         "group e",
