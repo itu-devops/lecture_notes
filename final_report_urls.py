4c013965d06f6e74cfc542430b6188506b67d3a4
Daniel Millard <75475183+DanielSMillard@users.noreply.github.com>
2024-05-22 23:18:56 +0200

    Add Group N report url to final_report_urls.py

--- final_report_urls.py
+++ final_report_urls.py
@@ -79,9 +79,9 @@ REPORT_URLS = [
     ],
     [
         "group n",
-        "<name>",
+        "MacOnTop",
         # Report Release URL:
-        "https://github.com/<gh_id>/<proj_id>/archive/refs/tags/<version_tag>.zip",
+        "https://github.com/itu-devops-groupn/itu-minitwit/blob/main/report/build/BSc_group_n.pdf",
     ],
     [
         "group o",
