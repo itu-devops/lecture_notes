f5bd9379895ad6c2a42cf06a184bd169976e5177
Sebastian <andersen_sebastian@yahoo.dk>
2025-01-31 13:56:51 +0100

    Update repositories.py

--- repositories.py
+++ repositories.py
@@ -71,8 +71,8 @@ GROUP_REPOS = [
     ],
     [
         "group k",
-        "<name>",
-        ["https://github.com/<organization|user>/<repository_name>"],
+        "Sad people",
+        ["https://github.com/DevOps-Sad-people/itu-minitwit"],
         "http(s)://<TBA>/<FrontEndURL>",
         "http(s)://<TBA>/<APIURL>",
     ],
