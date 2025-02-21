e5b1801a6ad990125dd42babf394a6852008f8e3
Christian Bank Lauridsen <94063787+ChrBank@users.noreply.github.com>
2025-02-21 16:24:08 +0100

    Update repositories.py

--- repositories.py
+++ repositories.py
@@ -66,8 +66,8 @@ GROUP_REPOS = [
         "group j",
         "The Pentuple",
         ["https://github.com/Grumlebob/The-Pentuple-MiniTwit"],
-        "http(s)://<TBA>/<FrontEndURL>",
-        "http(s)://<TBA>/<APIURL>",
+        "http://138.68.125.16:5001",
+        "http://138.68.125.16:5002",
     ],
     [
         "group k",
